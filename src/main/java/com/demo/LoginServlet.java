fb327c5f2ce77d37cb7f79da0237d7f14da559a6
ravi <ravi@gmail.com>
2021-12-15 23:05:32 +0530

    Added : Code to validate username

--- src/main/java/com/demo/LoginServlet.java
+++ src/main/java/com/demo/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.demo;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,7 +18,15 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        // Regex Pattern For UserName
+
         String user = request.getParameter("user");
+        String namePattern = "^[A-Z]{1}[a-z]{3,}$";
+        Pattern pattern = Pattern.compile(namePattern);
+        Matcher match = pattern.matcher(user);
+
+
         String pwd = request.getParameter("pwd");
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
